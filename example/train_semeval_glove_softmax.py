4cbe5ec3c1a9d1c27b202ff536533216bb68dabe
Maik Paixão <paixomaik5@gmail.com>
2020-05-04 16:03:41 -0300

    Update train_semeval_glove_softmax.py

--- example/train_semeval_glove_softmax.py
+++ example/train_semeval_glove_softmax.py
@@ -6,7 +6,7 @@ import numpy as np
 import json
 import opennre
 from opennre import encoder, model, framework
-from opennre.encoder import base_encoder.GloveEncoder
+from opennre.encoder.base_encoder import GloveEncoder
 
 ckpt = 'ckpt/semeval_cnn_softmax.pth.tar'
 wordi2d = json.load(open('../pretrain/glove/glove.6B.50d_word2id.json'))
