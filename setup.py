3ce1d0d1c5f1f1484b798da76d49515a41ed30f8
Maik Paixão <paixomaik5@gmail.com>
2020-05-04 16:14:48 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 with open("README.md", "r") as fh:
     setuptools.setup(
-        name='openlis',  
+        name='opennre',  
         version='0.1',
         author="Tianyu Gao",
         author_email="gaotianyu1350@126.com",
         description="An open source toolkit for relation extraction",
-        url="https://github.com/maikpaixao/openlis",
+        url="https://github.com/thunlp/opennre",
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
