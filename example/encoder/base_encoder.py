d4c7487e2eda624c686231cd8f4d767ee7b723b7
Maik Paixão <paixomaik5@gmail.com>
2020-05-04 16:21:19 -0300

    Update base_encoder.py

--- example/encoder/base_encoder.py
+++ example/encoder/base_encoder.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import sys
+sys.path.append("..")
 from ..tokenization import WordTokenizer
 
 class BaseEncoder(nn.Module):
