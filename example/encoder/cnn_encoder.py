f9af0c67c5d5d1399dec649839f456fe8f2b504b
Maik Paixão <paixomaik5@gmail.com>
2020-05-04 22:49:08 -0300

    Update cnn_encoder.py

--- example/encoder/cnn_encoder.py
+++ example/encoder/cnn_encoder.py
@@ -118,7 +118,7 @@ class WEEncoder(GloveEncoder):
         Return:
             (B, EMBED), representations for sentences
         """
-        x = self.word_embedding(token)
+        x = torch.cat([self.word_embedding(token)], 2)
         x = self.act(self.conv(x)) # (B, H, L)
         x = self.pool(x).squeeze(-1)
         x = self.drop(x)
