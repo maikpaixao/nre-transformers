9ec30780a4260187467ece479c5d003c9664fb60
Maik Luiz Paixão <paixomaik@outlook.com.br>
2020-05-10 09:04:07 -0300

    semantic encoder fix

--- example/encoder/cnn_encoder.py
+++ example/encoder/cnn_encoder.py
@@ -144,7 +144,7 @@ class SCNNEncoder(BaseEncoder):
                  activation_function=F.relu,
                  mask_entity=False):
 
-        super(CNNEncoder, self).__init__(token2id, max_length, hidden_size, word_size, position_size, blank_padding, word2vec, mask_entity=mask_entity)
+        super(SCNNEncoder, self).__init__(token2id, max_length, hidden_size, word_size, position_size, blank_padding, word2vec, mask_entity=mask_entity)
         self.drop = nn.Dropout(dropout)
         self.kernel_size = kernel_size
         self.padding_size = padding_size
