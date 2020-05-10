256bceae0502a8bede619ffe10aa307ff8536373
Maik Luiz Paixão <paixomaik@outlook.com.br>
2020-05-10 11:57:05 -0300

    m

--- example/encoder/cnn_encoder.py
+++ example/encoder/cnn_encoder.py
@@ -155,7 +155,7 @@ class SCNNEncoder(BaseEncoder):
 
     def forward(self, token):
         # Check size of tensors
-        x = self.word_embedding(token).unsqueeze(1)
+        x = self.word_embedding(token).unsqueeze(3)
         x = x.transpose(2, 1)
         x = self.act(self.conv(x)) # (B, H, L)
         x = self.pool(x).squeeze(-1)
