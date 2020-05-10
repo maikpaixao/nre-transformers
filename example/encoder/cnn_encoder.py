6073a3b63665af38cf4d11ce40b62859d44833fa
Maik Paixão <paixomaik5@gmail.com>
2020-05-10 14:42:31 -0300

    Update cnn_encoder.py

--- example/encoder/cnn_encoder.py
+++ example/encoder/cnn_encoder.py
@@ -150,7 +150,7 @@ class SCNNEncoder(BaseEncoder):
         self.padding_size = padding_size
         self.act = activation_function
         self.conv = nn.Conv1d(self.input_size, self.hidden_size, self.kernel_size, padding=self.padding_size)
-        self.pool = nn.AvgPool1d(self.kernel_size, stride=2, padding = padding=self.padding_size)
+        self.pool = nn.AvgPool1d(self.kernel_size, stride=2, padding=self.padding_size)
 
     def forward(self, token):
         # Check size of tensors
