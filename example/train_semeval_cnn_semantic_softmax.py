56348b54a4d6815fd589ccaa82b671770a9f1872
Maik Paixão <paixomaik5@gmail.com>
2020-05-10 14:54:50 -0300

    Update train_semeval_cnn_semantic_softmax.py

--- example/train_semeval_cnn_semantic_softmax.py
+++ example/train_semeval_cnn_semantic_softmax.py
@@ -4,22 +4,11 @@ import numpy as np
 import json
 import opennre
 from opennre import encoder, model, framework
-from encoder import cnn_encoder
 
-ckpt = 'ckpt/semeval_cnn_softmax.pth.tar'
-wordi2d = json.load(open('../pretrain/glove/glove.6B.50d_word2id.json'))
-word2vec = np.load('../pretrain/glove/glove.6B.50d_mat.npy')
+ckpt = 'ckpt/semeval_bert_softmax.pth.tar'
 rel2id = json.load(open('../benchmark/semeval/semeval_rel2id.json'))
-sentence_encoder = cnn_encoder.SCNNEncoder(token2id=wordi2d,
-                                             max_length=100,
-                                             word_size=50,
-                                             position_size=0,
-                                             hidden_size=100,
-                                             blank_padding=True,
-                                             kernel_size=3,
-                                             padding_size=1,
-                                             word2vec=word2vec,
-                                             dropout=0.5)
+sentence_encoder = opennre.encoder.BERTEncoder(
+    max_length=80, pretrain_path='../pretrain/bert-base-uncased')
 model = opennre.model.SoftmaxNN(sentence_encoder, len(rel2id), rel2id)
 framework = opennre.framework.SentenceRE(
     train_path='../benchmark/semeval/semantics/train.txt',
@@ -27,11 +16,10 @@ framework = opennre.framework.SentenceRE(
     test_path='../benchmark/semeval/semantics/test.txt',
     model=model,
     ckpt=ckpt,
-    batch_size=32,
-    max_epoch=100,
-    lr=0.1,
-    weight_decay=1e-5,
-    opt='sgd')
+    batch_size=64,
+    max_epoch=10,
+    lr=3e-5,
+    opt='adam')
 # Train
 framework.train_model(metric='micro_f1')
 # Test
