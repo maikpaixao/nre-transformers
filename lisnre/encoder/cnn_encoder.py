84eeee24b9df4946b621f0c8a7bd0a7fbfc260b8
Maik Luiz Paixão <paixomaik@outlook.com.br>
2020-05-11 15:20:39 -0300

    s

--- lisnre/encoder/cnn_encoder.py
+++ lisnre/encoder/cnn_encoder.py
@@ -64,7 +64,7 @@ class CNNEncoder(BaseEncoder):
                        self.pos1_embedding(pos1),
                        self.pos2_embedding(pos2),
                        self.word_embedding(xs),
-                       self.word_embedding(ys)], 2) # (B, L, EMBED)
+                       self.word_embedding(ys)], 4) # (B, L, EMBED)
         print(x.shape)
         x = x.transpose(1, 2) # (B, EMBED, L)
         x = self.act(self.conv(x)) # (B, H, L)
