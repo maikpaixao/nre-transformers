835fd4d6897be8664fe2e0b00fe9780cbd1fc856
Maik Paixão <paixomaik5@gmail.com>
2020-05-11 03:33:34 +0200

    Update cnn_encoder.py

--- example/encoder/cnn_encoder.py
+++ example/encoder/cnn_encoder.py
@@ -67,6 +67,7 @@ class CNNEncoder(BaseEncoder):
         x = self.act(self.conv(x)) # (B, H, L)
         x = self.pool(x).squeeze(-1)
         x = self.drop(x)
+        torch.save(tensor, 'file.pt')
         return x
 
     def tokenize(self, item):
