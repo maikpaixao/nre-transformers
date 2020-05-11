da2ec0429d006514d16cc35ee7816eed30f8d4a6
Maik Paixão <paixomaik5@gmail.com>
2020-05-11 03:31:51 +0200

    Update train_semeval_cnn_softmax.py

--- example/train_semeval_cnn_softmax.py
+++ example/train_semeval_cnn_softmax.py
@@ -4,12 +4,13 @@ import numpy as np
 import json
 import opennre
 from opennre import encoder, model, framework
+from encoder import cnn_encoder
 
 ckpt = 'ckpt/semeval_cnn_softmax.pth.tar'
 wordi2d = json.load(open('../pretrain/glove/glove.6B.50d_word2id.json'))
 word2vec = np.load('../pretrain/glove/glove.6B.50d_mat.npy')
 rel2id = json.load(open('../benchmark/semeval/semeval_rel2id.json'))
-sentence_encoder = opennre.encoder.CNNEncoder(token2id=wordi2d,
+sentence_encoder = cnn_encoder.CNNEncoder(token2id=wordi2d,
                                              max_length=100,
                                              word_size=50,
                                               position_size=0,
