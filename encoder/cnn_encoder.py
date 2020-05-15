da9ee932869080c920cddf8565a4f88697c1f866
maikpaixao <paixomaik@outlook.com.br>
2020-05-15 17:30:22 -0300

    ad

--- encoder/cnn_encoder.py
+++ encoder/cnn_encoder.py
@@ -56,7 +56,7 @@ class CNNEncoder(BaseEncoder):
         if self.e_chunks:
             x = torch.cat([x, self.word_embedding(chunks)], 2)
         if self.e_semantics:
-            semantics = torch.cat([self.word_embedding(ses1), self.word_embedding(ses2)], 2)
+            semantics = torch.cat([self.word_embedding(ses1), self.word_embedding(ses2)], 1)
             #semantics = semantics.unsqueeze()
 
         x = x.transpose(1, 2)
