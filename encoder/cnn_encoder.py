2aa5096368eb61a56ae80771408d7df5d207beb4
maikpaixao <paixomaik@outlook.com.br>
2020-05-27 07:46:31 -0300

    fixed index error path

--- encoder/cnn_encoder.py
+++ encoder/cnn_encoder.py
@@ -58,7 +58,7 @@ class CNNEncoder(BaseEncoder):
         if self.e_position:
             x = torch.cat([x, self.pos1_embedding(pos1), self.pos2_embedding(pos2)], 2)
         elif self.e_path:
-            x = torch.cat([x, self.path_embedding(path)], 2)
+            x = torch.cat([x, self.word_embedding(path)], 2)
         elif self.e_chunks:
             x = torch.cat([x, self.word_embedding(chunks)], 2)
         elif self.e_semantics:
